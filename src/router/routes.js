9f705e27d98627f6fca1d439bde062216c1ce959
Marco Marson <7435285+marcomarson@users.noreply.github.com>
2020-04-09 00:16:30 -0300

    Changes on routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,8 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-      { path: 'dashboard', component: () => import('pages/DetailedDashboard.vue') }
+      { path: 'dashboard', component: () => import('pages/DetailedDashboard.vue') },
+      { path: 'detaileddashboard', component: () => import('pages/Dashboard.vue') }
     ]
   }
 ]
